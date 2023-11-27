594aad3febc3e0698eee47bbd85ec64a41aae09c
marcus-wrrn <marcuswrrn@gmail.com>
2023-11-26 22:39:43 -0500

    Added Base model

--- src/model.py
+++ src/model.py
@@ -42,6 +42,82 @@ class SentenceEncoder():
         return F.normalize(sentence_embeddings, p=2, dim=1) if normalize else sentence_embeddings
 
 
+class SentenceEncoder(nn.Module):
+    def __init__(self, model_name='all-mpnet-base-v2'):
+        super(SentenceEncoder, self).__init__()
+
+        self.name = model_name
+        self.encoder = SentenceTransformer(self.name)
+        self.fc = nn.Sequential(
+            nn.Linear(768, 500),
+            nn.ReLU(),
+            nn.Linear(500, 256),
+        )
+    
+    def forward(self, text: torch.Tensor):
+        encodings = self.encoder.encode(text, convert_to_tensor=True)
+        out = self.fc(encodings)
+        return out
+
+
+class EncoderLayer(nn.Module):
+    """
+    A neural network module for encoding sentences using the SentenceTransformer model.
+
+    This class supports encoding sentences with different labels (e.g., positive, negative, neutral, assassin).
+    Each label type can be optionally included in the encoding process.
+
+    Attributes:
+        pos_encoder (SentenceTransformer): Encoder for positive sentences.
+        neg_encoder (SentenceTransformer): Encoder for negative sentences.
+        neutral_encoder (SentenceTransformer): Encoder for neutral sentences.
+        assassin_encoder (SentenceTransformer): Encoder for assassin words.
+    """
+
+    def __init__(self, has_pos=True, has_neg=True, has_neutral=True, has_assassin=False) -> None:
+        super().__init__()
+
+        model_name = 'all-mpnet-base-v2'
+        self.encoders = {
+            'pos': SentenceEncoder(model_name) if has_pos else None,
+            'neg': SentenceEncoder(model_name) if has_neg else None,
+            'neutral': SentenceEncoder(model_name) if has_neutral else None,
+            'assassin': SentenceEncoder(model_name) if has_assassin else None
+        }
+
+    def forward(self, pos_sents='', neg_sents='', neutral_sents='', assassin_word=''):
+        encoded = {
+            'pos': self.encoders['pos'](pos_sents) if self.encoders['pos'] else None,
+            'neg': self.encoders['neg'](neg_sents) if self.encoders['neg'] else None,
+            'neutral': self.encoders['neutral'](neutral_sents) if self.encoders['neutral'] else None,
+            'assassin': self.encoders['assassin'](assassin_word) if self.encoders['assassin'] else None
+        }
+
+        return encoded['pos'], encoded['neg'], encoded['neutral'], encoded['assassin']
+
+class SimpleEncoderLayer(nn.Module):
+    def __init__(self, model_name='all-mpnet-base-v2'):
+        super().__init__()
+        self.name = model_name
+        
+        self.pos_encoder = SentenceEncoder(model_name)
+        self.neg_encoder = SentenceEncoder(model_name)
+
+        self.fc = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 758)
+        )
+    
+    def forward(self, pos_texts, neg_texts):
+        pos_emb = self.pos_encoder(pos_texts)
+        neg_emb = self.neg_encoder(neg_texts)
+
+        concatenated = torch.cat((pos_emb, neg_emb), 0)
+        return self.fc(concatenated)
+
+
+
 class CodeGiver(nn.Module):
     def __init__(self, model_name="microsoft/deberta-base", device=torch.device('cpu')) -> None:
         super().__init__()
