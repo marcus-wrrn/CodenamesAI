8d6b4a68e228ca5b797c3d12cedf08823961e539
marcus-wrrn <marcuswrrn@gmail.com>
2023-12-09 11:50:28 -0500

    Added VectorSearch object

--- src/test.py
+++ src/test.py
@@ -3,38 +3,28 @@ from dataset import CodeGiverDataset
 import torch
 import torch.nn.functional as F
 import argparse
-import faiss
 import numpy as np
-
-def get_device(is_cuda: str):
-    if (is_cuda.lower() == 'y' and torch.cuda.is_available()):
-        return torch.device("cuda")
-    return torch.device("cpu")
+from vector_search import VectorSearch
+import utils.utilities as utils
 
 def calc_cos_score(anchor, pos_encs, neg_encs):
     pos_score = F.cosine_similarity(anchor, pos_encs, dim=1)
-    #pos_score = torch.mean(pos_score, dim=1)
-
     neg_score = F.cosine_similarity(anchor, neg_encs, dim=1)
-    #neg_score = torch.mean(neg_score, dim=1)
-
     
-    print(f"Pos Score: {pos_score}")
-    print(f"Neg Score: {neg_score}\n")
+    return pos_score, neg_score
 
 @torch.no_grad()
 def test_loop(model: SimpleCodeGiver, dataset: CodeGiverDataset, device: torch.device):
-    vocab_words, vocab_embeddings = dataset.get_vocab()
-    vocab_embeddings = np.array(vocab_embeddings).astype(np.float32)
-    index = faiss.IndexHNSWFlat(768, 32)
-    index.add(vocab_embeddings)
-
+    # Initialize vector search data struct
+    vectorDB = VectorSearch(dataset)
     for data in dataset:
         pos_sents, neg_sents, pos_embs, neg_embs = data
         pos_embs, neg_embs = pos_embs.to(device), neg_embs.to(device)
         logits = model(pos_sents, neg_sents)
-        D, I = index.search(logits.cpu().numpy(), 20)
-        words = [vocab_words[i] for i in I[0]]
+
+        words, embeddings, D = vectorDB.search(logits)
+        words = words[0]
+
         for i, word in enumerate(words):
             print(f"{i + 1}: {word}: {D[0][i]}")
         
@@ -43,17 +33,17 @@ def test_loop(model: SimpleCodeGiver, dataset: CodeGiverDataset, device: torch.d
         for i, word in enumerate(words):
             if word not in pos_sents.split(' '):
                 out_word = word
-                anchor = torch.tensor(vocab_embeddings[I[0][i]]).unsqueeze(0)
+                anchor = torch.tensor(embeddings[0][i]).unsqueeze(0)
                 anchor = anchor.to(device)
                 break
         print(f"Output: {out_word}")
         print(f"Positive: {pos_sents}\nNegative: {neg_sents}")
-        calc_cos_score(anchor, pos_embs, neg_embs)
-        print('')
+        pos_score, neg_score = calc_cos_score(anchor, pos_embs, neg_embs)
+        print(f"")
 
 
 def main(args):
-    device = get_device(args.cuda)
+    device = utils.get_device(args.cuda)
     model = SimpleCodeGiver()
     model.load_state_dict(torch.load(args.m))
     model.to(device)
---
+++ src/vector_search.py
@@ -0,0 +1,29 @@
+import numpy as np
+from dataset import CodeGiverDataset
+import faiss
+import torch
+
+
+class VectorSearch():
+    def __init__(self, dataset: CodeGiverDataset, n_dim=768, n_neighbours=32) -> None:
+        self.vocab_words, self.vocab_embeddings = dataset.get_vocab()
+        self.vocab_words = np.array(self.vocab_words)
+        # Process embeddings
+        self.vocab_embeddings = np.array(self.vocab_embeddings).astype(np.float32)
+        # Initialize index + add embeddings
+        self.index = faiss.IndexHNSWFlat(n_dim, n_neighbours)
+        self.index.add(self.vocab_embeddings)
+    
+    def search(self, logits: torch.Tensor, num_results=20):
+        # detach tensor from device and convert it to numpy for faiss compatibility
+        search_input = logits.cpu().numpy()
+        # D: L2 distance from input, I: index of result
+        D, I = self.index.search(search_input, num_results)
+        # Map index values to words
+        words = self.vocab_words[I]
+        embeddings = self.vocab_embeddings[I]
+        return words, embeddings, D
+
+    def index_to_words(self, index):
+        return self.vocab_words[index]
+    
